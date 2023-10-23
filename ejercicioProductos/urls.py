d3fb38ec7e2f54c1640687246bf6a230bfa0bd2b
Pierre <piercares@gmail.com>
2023-10-23 14:51:39 -0300

    app2 creada y conectada

--- ejercicioProductos/urls.py
+++ ejercicioProductos/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from productosApp.views import *
+from App2.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
